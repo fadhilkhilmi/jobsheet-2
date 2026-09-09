0384a91af5863df32e21ed0e1d4897d9a3253604
fadhilkhilmi <fadhilkhilmi71@gmail.com>
2026-09-09 11:23:00 +0700

    latihan2

---
+++ Bagian 1/latihan2.js
@@ -0,0 +1,40 @@
+const cart = [
+    {
+        title: "Laptop",
+        price: 1000,
+        discountPercent: 10
+    },
+    {
+        title: "Mouse",
+        price: 20,
+        discountPercent: 5
+    },
+    {
+        title: "Keyboard",
+        price: 50,
+        discountPercent: 0
+    }
+];
+
+function applyDiscounts(cart) {
+    const result = [];
+
+    for (const item of cart) {
+
+        const finalPrice = calculateDiscountedPrice(
+            item.price,
+            item.discountPercent
+        );
+
+        result.push({
+            title: item.title,
+            price: item.price,
+            discountPercent: item.discountPercent,
+            finalPrice: finalPrice
+        });
+    }
+
+    return result;
+}
+
+console.log(applyDiscounts(cart));
