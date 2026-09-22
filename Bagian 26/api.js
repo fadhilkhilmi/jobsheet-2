cb99bcbae5f5cb2e187906b2ef163c5787f7f578
fadhilkhilmi <fadhilkhilmi71@gmail.com>
2026-09-22 07:30:29 +0700

    selesai

---
+++ Bagian 26/api.js
@@ -0,0 +1,19 @@
+
+
+const BASE_URL = "https://dummyjson.com/products";
+
+export async function fetchProducts(limit = 100) {
+  try {
+    const response = await fetch(`${BASE_URL}?limit=${limit}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.products; // { products, total, skip, limit } -> ambil products saja
+  } catch (error) {
+    console.error("Gagal mengambil data:", error);
+    throw error;
+  }
+}
